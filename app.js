41ad8ce9a645bb2d0b4542fae65f4bd2a015f0f6
liudawei <liudawei@smzdm.com>
2021-05-18 10:41:21 +0800

    init

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ app.use(bodyParser());
 app.use(async (ctx) => {
     //当请求时GET请求时，显示表单让用户填写
     if (ctx.method === 'GET') {
+        console.log('GET');
         ctx.body = 'GET';
         //当请求时POST请求时
     } else if (ctx.method === 'POST') {
         //post请求
-        let pastData = ctx.request.body
+        let pastData = ctx.request.body;
+        console.log('post', pastData);
         ctx.body = pastData;
     } else {
         ctx.body = '<h1>404!</h1>';
