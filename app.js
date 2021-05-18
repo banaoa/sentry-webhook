66aca9267949c2f5b84a7b9d00d6adc0ea4eeb4f
liudawei <liudawei@smzdm.com>
2021-05-18 14:15:30 +0800

    init

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser')
 const app = new Koa();
+const ChatBot = require('dingtalk-robot-sender');
+const robot = new ChatBot({
+    webhook: 'https://oapi.dingtalk.com/robot/send?access_token=fe775bb14aa7ca55601e11e1675c35f2b2b393f14a33f59ca78fc067d9356415'
+});
+
 app.use(bodyParser());
 
 app.use(async (ctx) => {
@@ -11,8 +16,24 @@ app.use(async (ctx) => {
         //当请求时POST请求时
     } else if (ctx.method === 'POST') {
         //post请求
-        let pastData = ctx.request.body;
-        console.log('post', pastData);
+        let postData = ctx.request.body;
+        console.log('post', postData);
+        // postData.action
+        // postData.data
+        // postData.data.issue
+        // postData.data.issue.title
+
+        let textContent = {
+            "msgtype": "text",
+            "text": {
+                "content": postData.data.issue.title
+            }
+        }
+        robot.send(textContent)
+            .then((res) => {
+                // TODO
+            });
+
         ctx.body = pastData;
     } else {
         ctx.body = '<h1>404!</h1>';
